a6a0ea9ea258b5a4a833247b8d9d5f61e62f248d
Nishanth Vijayan <nishanththegr8@gmail.com>
2016-03-02 23:52:06 +0530

    Replace innerHTML method with the ugly code because of security requirements.See: https://developer.mozilla.org/en-US/Add-ons/Overlay_Extensions/XUL_School/DOM_Building_and_HTML_Insertion

--- data/replacer.js
+++ data/replacer.js
@@ -1,1 +1,4 @@
-document.body.innerHTML = 	document.body.innerHTML.replace(/trump/g, 'drumpf').replace(/Trump/g, 'Drumpf');
+var textNode, walk=document.createTreeWalker(document,NodeFilter.SHOW_TEXT,null,false);
+while(textNode=walk.nextNode()) {
+    textNode.nodeValue = textNode.nodeValue.replace(/trump/g, 'drumpf').replace(/Trump/g, 'Drumpf');
+}
